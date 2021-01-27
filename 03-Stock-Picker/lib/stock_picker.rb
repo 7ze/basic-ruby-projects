857393491851b85130c934dbf2137ecf26c077b4
The-sad-philosopher <shinudonney@tuta.io>
2021-01-27 09:50:58 +0530

    ✨ Solve the problem

--- 03-Stock-Picker/lib/stock_picker.rb
+++ 03-Stock-Picker/lib/stock_picker.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 def stock_picker(stocks)
-  # logic
+  profit = {}
+  stocks.each_with_index do |price1, i|
+    stocks.each_with_index do |price2, j|
+      profit[[i, j]] = price2 - price1 if j > i
+    end
+  end
+  max_profit = profit.max_by(&:last)
+  "Buy on day #{max_profit[0][0]} and sell on day #{max_profit[0][1]} for a profit of $#{max_profit.last}"
 end
-
